f279fbeb95413cafc696c5092adb5341e03c037f
KimJihun13157ff <91823003+KimJihun13157ff@users.noreply.github.com>
2022-09-14 23:46:00 +0900

    assign1 Ex3 fix
    I think it's bad that the function changes the list at Ex3

--- Main.scala
+++ Main.scala
@@ -17,46 +17,52 @@ object Main {
    * Exercise 1
    */
   def pascal(c: Int, r: Int): Int = {
-    def Factorial(n: Int, res : Int = 1): Int = {
-      if( n == 0)
+    def Factorial(n: Int, res: Int = 1): Int = {
+      if (n == 0)
         res
       else
-        Factorial(n-1, res * n)
+        Factorial(n - 1, res * n)
     }
-    if(c > r || c < 0 || r < 0) 0
+
+    if (c > r || c < 0 || r < 0) 0
     else
-      Factorial(r) / (Factorial(c) * Factorial(r-c))
+      Factorial(r) / (Factorial(c) * Factorial(r - c))
   }
 
   /**
    * Exercise 2
    */
-  def balance(chars: List[Char]): Boolean =
-    {
-      def balanceCounter(chars: List[Char], count :Int = 0) : Boolean = {
-        if (chars.isEmpty || count < 0)
-          if (count == 0) true
-          else false
-        else if (chars.head == '(')
-          balanceCounter(chars.tail, count + 1)
-        else if (chars.head == ')')
-          balanceCounter(chars.tail, count - 1)
-        else
-          balanceCounter(chars.tail, count)
+  def balance(chars: List[Char]): Boolean = {
+    def balanceCounter(chars: List[Char], count: Int = 0): Boolean = {
+      if (chars.isEmpty || count < 0)
+        if (count == 0) true
+        else false
+      else if (chars.head == '(')
+        balanceCounter(chars.tail, count + 1)
+      else if (chars.head == ')')
+        balanceCounter(chars.tail, count - 1)
+      else
+        balanceCounter(chars.tail, count)
 
-      }
-        balanceCounter(chars)
     }
+
+    balanceCounter(chars)
+  }
 
   /**
    * Exercise 3
    */
   def countChange(money: Int, coins: List[Int]): Int = {
-    if (money == 0)
-      1
-    else if (money < 0 || coins.isEmpty)
-      0
-    else
-      countChange(money - coins.head, coins) + countChange(money, coins.tail)}
+    def countRecursive(money: Int, coins: List[Int]): Int = {
+      if (money == 0)
+        1
+      else if (money < 0 || coins.isEmpty)
+        0
+      else
+        countRecursive(money - coins.head, coins) + countRecursive(money, coins.tail)
+    }
+
+    countRecursive(money, coins)
+  }
 }
 
